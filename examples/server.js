0ccc9759acd2b9e340148c65764b9432bc12a223
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2013-04-18 17:07:38 +0800

    Add forward + renaming stuff

--- examples/server.js
+++ examples/server.js
@@ -10,19 +10,26 @@
 var MB = require('..');
 
 var mb = new MB({
-    log_port : 3044,
+    listen_port : 3044,
     keep_it_alive : true,
     router : [{
 	route : 'log:*',
-	store : 'ConsoleBus'
+	dest : 'ConsoleTransport'
     },{
 	route : 'carcass:*',
-	store : 'ConsoleBus'
+	dest : 'ConsoleTransport'
     },{
 	route : 'reds:*',
-	store : 'RedisBus',
-	db : {
+	dest : 'RedisTransport',
+	conf : {
 	    port : 6379,
+	    host : '127.0.0.1'
+	}
+    },{
+	route : 'forward:*',
+	dest : 'AxonForward',
+	conf : {
+	    port : 6380,
 	    host : '127.0.0.1'
 	}
     }]
